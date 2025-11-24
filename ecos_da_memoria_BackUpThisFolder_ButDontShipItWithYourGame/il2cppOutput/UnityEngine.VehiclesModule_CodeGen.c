570df9c3242758dd3416cf39980db1548f31fa27
Dimitri Zenaro <dimitrizenaro12@gmail.com>
2025-11-24 11:44:23 -0300

    hn9jhrtj0rht

---
+++ ecos_da_memoria_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.VehiclesModule_CodeGen.c
@@ -0,0 +1,51 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void WheelCollider_get_steerAngle_mF0A004554F1509F1DA233AC71A7A2C9C36CC9EE5 (void);
+extern void WheelCollider_set_steerAngle_m7BF83B27D8956355F873537939BE9F35CF3113C3 (void);
+extern void WheelCollider__ctor_mD44B2C97960274797DABD055BBEB18E664DAD906 (void);
+extern void WheelCollider_get_steerAngle_Injected_m7D5107032A83BC17496DC445774BA15076612714 (void);
+extern void WheelCollider_set_steerAngle_Injected_m1D19C28872BFFC9C692FA4F50454A3400D5ACC20 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	WheelCollider_get_steerAngle_mF0A004554F1509F1DA233AC71A7A2C9C36CC9EE5,
+	WheelCollider_set_steerAngle_m7BF83B27D8956355F873537939BE9F35CF3113C3,
+	WheelCollider__ctor_mD44B2C97960274797DABD055BBEB18E664DAD906,
+	WheelCollider_get_steerAngle_Injected_m7D5107032A83BC17496DC445774BA15076612714,
+	WheelCollider_set_steerAngle_Injected_m1D19C28872BFFC9C692FA4F50454A3400D5ACC20,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	16511,
+	12730,
+	16617,
+	26944,
+	23955,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_VehiclesModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_VehiclesModule_CodeGenModule = 
+{
+	"UnityEngine.VehiclesModule.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
