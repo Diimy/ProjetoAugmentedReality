9e575d4e2f17d88c5e528bab305693d333b41dfb
Dimitri Zenaro <dimitrizenaro12@gmail.com>
2025-11-24 23:58:35 -0300

    Fix: removed Library from repo

--- Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/2t4b4h3h/logs/arm64-v8a/build_command_game.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Program Files\\Unity\\Hub\\Editor\\6000.3.0b10\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\dimit\\Documents\\GitHub\\ProjetoAugmentedReality\\.utmp\\RelWithDebInfo\\2t4b4h3h\\arm64-v8a" ^
-  game
--- Library/PackageCache/com.unity.xr.arfoundation@f2cbfcba674a/Runtime/ARSubsystems/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-#if UNITY_EDITOR
-[assembly: InternalsVisibleTo("Unity.XR.ARSubsystems.Editor")]
-[assembly: InternalsVisibleTo("Unity.XR.ARSubsystems.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.XR.ARFoundation.Runtime.Tests")]
-#endif
